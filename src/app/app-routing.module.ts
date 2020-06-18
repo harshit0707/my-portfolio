895233f24f9f4c9e4c6fc0327b76fa1896490a2d
Harshit Agrawal <harshit.agrawal@diligenta.co.uk>
2020-06-18 22:33:07 +0530

    routing changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomePageComponent } from './components/home-page/home-page.component';
 
 const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', component: HomePageComponent }
 ];
 
