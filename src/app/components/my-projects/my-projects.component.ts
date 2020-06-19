fc1a0e89f05e12afeaa41b965bd99846650eab2b
Harshit Agrawal <harshit.agrawal@diligenta.co.uk>
2020-06-19 22:31:42 +0530

    added few projects of mine

--- src/app/components/my-projects/my-projects.component.ts
+++ src/app/components/my-projects/my-projects.component.ts
@@ -7,9 +7,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MyProjectsComponent implements OnInit {
 
+  myProjects: Array<Object>;
   constructor() { }
 
   ngOnInit(): void {
+    this.initializeProjects();
+  }
+
+  initializeProjects() {
+    this.myProjects = [
+      {'title': 'Mosaic Blogging Website', 'description': 'My first project build during college. It\'s all about blogging, blobgging and blogging.'},
+      {'title': 'Blockchain Healthcare System', 'description': 'Healthcare system for patients built on ehthereum blockchain for anonymity and immutability.'},
+      {'title': 'Online Portfolio', 'description': 'The website which you are on currently!! It is work on progress.'}
+    ]
   }
 
 }
